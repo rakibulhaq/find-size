6aac5d98923dc5003f21c909838f9f442f1d2cd2
rakibulhaq <haq.rakibul@gmail.com>
2019-10-08 10:04:25 +0600

    directory cleanup, console logs removed, postman testing script added

--- app/api/v1/find-size/find-size.handler.js
+++ app/api/v1/find-size/find-size.handler.js
@@ -34,17 +34,11 @@ class FindSizeHandler {
         //if the obj has a content property than it is a directory object
         if (obj[elem].hasOwnProperty("content")) {
           //it is a directory object, add size property to it
-          console.log("it is a Directory: " , obj[elem]['name']);
-          console.log("sum before this Directory: ", localSum);
           obj[elem]["size"] = localSum + this.sum(obj[elem]["content"], localSum);
           localSum = obj[elem]["size"];
-          console.log("sum after this Directory: ", localSum);
         } else {
           //the obj is file object, so it has size property
-          console.log("it is a file: ", obj[elem]['name']);
-          console.log("sum before this file run down: ", localSum);
           localSum = localSum + obj[elem]["size"];
-          console.log("sum after this file run down: ", localSum);
         }
       }
     return localSum;
